0aacb830ecc871f7e246e02f2ea6385c2c41355e
Waruna <114459188+Waruna418@users.noreply.github.com>
2023-04-03 23:56:19 +0530

    upload the code

---
+++ password-reset-system-with-php/inc/config.php
@@ -0,0 +1,14 @@
+<?php
+class dbConfig {
+    protected $serverName;
+    protected $userName;
+    protected $password;
+    protected $dbName;
+    function dbConfig() {
+        $this -> serverName = 'localhost';
+        $this -> userName = 'codewit8_damn_d';
+        $this -> password = "G*5EF,mAvyH3";
+        $this -> dbName = "codewit8_webdamn_demo";
+    }
+}
+?>
